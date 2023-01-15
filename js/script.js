a268004a32dd399e745e8ef2a71bb743b590dd65
zaman20 <adorzaman18@gmail.com>
2023-01-16 00:52:10 +0600

    Menu linked and icon changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 //alert("hello");
+$('.on-change').on('change',function(){
+    let data =$(this).val();
+    if(data == 0){$('.task-list tr').show();}
+
+    else{
+   // alert(data);
+   $('.task-list tr').hide();
+   $('.task-list td ').filter(function(){
+    return $(this).text()== data;
+   }).parent().show();}
+});
+
+let input = document.querySelector(".search-field");
+input.addEventListener('keyup',(e)=>{
+    if(e.keyCode === 13){
+        let data = e.target.value;
+    $('.task-list tr').hide();
+    $('.task-list td ').filter(function(){
+        return $(this).text()==data;
+    }).parent().show();}
+    
+})
